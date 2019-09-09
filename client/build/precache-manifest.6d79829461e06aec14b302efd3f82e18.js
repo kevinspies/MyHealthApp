761f324d957e6f8921103d977dd5e2e4b5f22a6d
Kevin Spies <kevin.spies@gmail.com>
2019-09-08 20:13:20 -0400

    commiting production build - version 1, app not complete, mostly a test production

---
+++ client/build/precache-manifest.6d79829461e06aec14b302efd3f82e18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3aba193b9ca7eda94fb7",
+    "url": "/static/js/main.de72fee0.chunk.js"
+  },
+  {
+    "revision": "6a18c8541aa3984ec0bf",
+    "url": "/static/js/2.6d48bfa6.chunk.js"
+  },
+  {
+    "revision": "3aba193b9ca7eda94fb7",
+    "url": "/static/css/main.f4c475cd.chunk.css"
+  },
+  {
+    "revision": "eb12acafa3c7088a3ee00b3bf89a4c70",
+    "url": "/index.html"
+  }
+];
